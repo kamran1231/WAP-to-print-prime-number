bb3a91f9cbb6286c3fde5de15922d14882d8fa1b
kamran khan <48378125+kamran1231@users.noreply.github.com>
2020-07-23 01:43:21 +0530

    Add files via upload

---
+++ primenumber.py
@@ -0,0 +1,13 @@
+
+lower = int(input('Enter lower range: '))
+upper = int(input('Enter the upper value: '))
+
+
+for num in range(lower,upper+1):
+    if num > 1:
+        for i in range(2,num):
+            if num % i == 0:
+                break
+        else:
+            print(num,end=' ')
+
